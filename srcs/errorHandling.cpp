b806e69c5bd4428d1e90d1ea1cd4199d242062b1
Kid-Seven-7 <joseph.ngoma@moov.life>
2018-07-03 13:25:25 +0200

    moved error handling into it's own file

---
+++ srcs/errorHandling.cpp
@@ -0,0 +1,40 @@
+#include "includes/abstractVM.hpp"
+
+void errorHandler(std::string arr[5], int parseCode){
+	if (parseCode != 0){
+		const char *lineNo = arr[1].c_str();
+		const char *inst = arr[2].c_str();
+		const char *dataType = arr[3].c_str();
+		const char *dataVal = arr[4].c_str();
+		switch (parseCode){
+			case INVALID_INST:
+				std::cout << "Line #" << lineNo << " Invalid instruction : ";
+				std::cout <<" \033[1;31m"<< inst << "\033[0m " << dataType << " " << dataVal << "\n";
+				exit(1);
+				break;
+			case INVALID_DATATYPE:
+				std::cout << "Line #" << lineNo << " Invalid data type : ";
+				std::cout << " " << inst << " " <<"\033[1;31m"<< dataType << "\033[0m " << " " << dataVal << '\n';
+				exit(1);
+				break;
+			case INVALID_DATAVALUE:
+				std::cout << "Line #" << lineNo << " Invalid data value : ";
+				std::cout<< " " << inst << " " << dataType << " " <<"\033[1;31m"<< dataVal << "\033[0m " << '\n';
+				exit(1);
+				break;
+			case INVALID_FORMAT:
+				std::cout << "Invalid format passed " << '\n';
+				exit(1);
+				break;
+			case VALUE_EXCEEDS_LIMIT:
+				std::cout << "Line #" << lineNo <<" \033[1;31m"<< dataVal << "\033[0m exceeds \033[1;33m"<<dataType<<"\033[0m limit";
+				exit(1);
+				break;
+			default:
+				std::cout << "\033[1;31mAn unknown error occurred\033[0m" << lineNo << '\n';
+				std::cout << inst << " " << dataType << " " << dataVal << '\n';
+				exit(1);
+				break;
+		}
+	}
+}
